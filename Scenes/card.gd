9ce989264b8ac3ea8692a66f160014ca57fff93c
Ricky <rjromanach@gmail.com>
2023-04-23 02:25:39 -0500

    Dragging logic: stacks by color and descending number
    Added outline to cards for better clarity.
    Cards can only be dragged in a stack if they are alternating in color and descending in number. Cards can only be placed on top of a card that is greater than them by one value.
    Also changed column's add_card function to be data type relative for ease of use in the place_stack function later.

--- Scenes/card.gd
+++ Scenes/card.gd
@@ -11,6 +11,7 @@ var is_being_dragged = false
 var parent_column
 var offset
 var dragged_stack
+var detected_column = null
 
 var column_position
 
@@ -29,9 +30,9 @@ func _process(delta):
 				dragged_stack[i].position.y += (card_spacing * i)
 		else:
 			is_being_dragged = false
-			for card in dragged_stack:
-				card.get_parent().remove_child(card)
-				parent_column.add_card(card)
+			$DragArea.set_deferred("disabled", true)
+			$CardArea.set_deferred("disabled", false)
+			place_stack()
 			get_tree().get_root().get_node("Main/Table").toggle_action_happening()
 
 
@@ -39,8 +40,49 @@ func on_click():
 	if !get_tree().get_root().get_node("Main/Table").is_action_happening():
 		get_tree().get_root().get_node("Main/Table").toggle_action_happening()
 		parent_column = get_parent()
-		dragged_stack = parent_column.remove_card(self)
-		offset = get_viewport().get_mouse_position() - (parent_column.position + position)
-		for card in dragged_stack:
-			card.reparent(get_parent().get_parent())
-		is_being_dragged = true
+		if can_drag_stack(parent_column.get_card_stack(self)):
+			$CardArea.set_deferred("disabled", true)
+			$DragArea.set_deferred("disabled", false)
+			dragged_stack = parent_column.remove_card(self)
+			offset = get_viewport().get_mouse_position() - (parent_column.position + position)
+			for card in dragged_stack:
+				card.reparent(get_parent().get_parent())
+			is_being_dragged = true
+		else:
+			get_tree().get_root().get_node("Main/Table").toggle_action_happening()
+
+
+func can_drag_stack(stack):
+	var can_move = true
+	if stack.size() == 1:
+		return can_move
+	
+	for i in range(0, stack.size() - 1):
+		if stack[i].color != stack[i+1].color: #opposite color
+			if stack[i].value == stack[i+1].value + 1: #decreasing in value
+				continue
+		can_move = false
+	return can_move
+
+
+func place_stack():
+	for card in dragged_stack:
+		card.get_parent().remove_child(card)
+		
+	if detected_column != null:
+		if detected_column.get_top_card().color != dragged_stack.back().color:
+			if detected_column.get_top_card().value == dragged_stack.back().value + 1:
+				detected_column.add_card(dragged_stack)
+				return
+	
+	parent_column.add_card(dragged_stack)
+
+
+func _on_area_entered(area):
+	if area.name.contains("Column"):
+		detected_column = area
+
+
+func _on_area_exited(area):
+	if is_being_dragged and area.name.contains("Column"):
+		detected_column = null
--- Scenes/column.gd
+++ Scenes/column.gd
@@ -14,13 +14,28 @@ func _process(delta):
 	pass
 
 
+#This is function makes it easier in other places when you don't know if your card is a single or a stack.
+#It prevents me from having to write for-loops everywhere
+func add_card(data):
+	match typeof(data):
+		TYPE_ARRAY:
+			add_cards(data)
+		TYPE_OBJECT:
+			add_card_single(data)
+
+
 # This function is expecting the specified card to have no parent before use
-func add_card(card):
+func add_card_single(card):
 	add_child(card)
 	card.column_position = cards.size()
 	card.position = Vector2(0, cards.size() * card_spacing)
 	card.column_position = cards.size()
 	cards.append(card)
+
+
+func add_cards(cards):
+	for card in cards:
+		add_card_single(card)
 
 
 #Returns an array, in order from the clicked card to the topmost card, holding the stack of cards that was just grabbed
@@ -30,3 +45,18 @@ func remove_card(card):
 		stack.append(cards.pop_back())
 	stack.reverse()
 	return stack
+
+
+func get_card_stack(card):
+	var stack = []
+	for i in range(card.column_position, cards.size()): #each card's column_position value is not updated while being dragged since it will never be used
+		stack.append(cards[i])
+	return stack
+
+
+func get_top_card():
+	return cards.back() if cards.size() > 0 else null
+	
+
+func _on_area_entered(area):
+	print(str("detector: ", self.name, "\tdetected: ", area))
